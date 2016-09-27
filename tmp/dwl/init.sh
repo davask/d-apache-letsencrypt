e5e0ed6346e25794c7f6fb9793ff10d98bbbce23
davask <davask.42@gmail.com>
2016-09-27 22:28:25 +0200

    Automatic update 2.4-u16.04

--- tmp/dwl/init.sh
+++ tmp/dwl/init.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+. /tmp/dwl/envvar.sh
 . /tmp/dwl/user.sh
 . /tmp/dwl/ssh.sh
 echo ">> Ubuntu initialized";
@@ -19,5 +20,8 @@ echo ">> apache2 initialized";
 
 . /tmp/dwl/certbot.sh
 echo ">> certbot initialized";
+
+. /tmp/dwl/permission.sh
+echo ">> permission assigned";
 
 . /tmp/dwl/keeprunning.sh
